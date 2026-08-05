f9d74aa7e7451800b863fde14a235431ed8608b1
Saranya-235 <srisaranya10m@gmail.com>
2026-08-05 23:12:16 +0530

    Made Mistake in saucedemo Test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,5 +3,5 @@ test("login saucedemo", async({page})=>{
     await page.goto("https://www.saucedemo.com/")
     await page.locator('#user-name').fill("standard_user")
     await page.locator('#password').fill("secret_sauce")
-    await page.click('#login-bu')
+    await page.click('#login-b')
 })
