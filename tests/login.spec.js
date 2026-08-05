694deb379eb7ae6ee62650064b22a1440fbd4218
Saranya-235 <srisaranya10m@gmail.com>
2026-08-05 23:24:35 +0530

    It should Fail Saucedemo Login Now

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,5 +3,5 @@ test("login saucedemo", async({page})=>{
     await page.goto("https://www.saucedemo.com/")
     await page.locator('#user-name').fill("standard_user")
     await page.locator('#password').fill("secret_sauce")
-    await page.locator('#abcd').click()
+    await page.locator('#abcd_12').click()
 })
