7dd9d6b15b7e825c7082d8330dac4e114df3ce4e
Saranya-235 <srisaranya10m@gmail.com>
2026-08-08 00:04:39 +0530

    changed url

--- tests/launch.spec.js
+++ tests/launch.spec.js
@@ -1,7 +1,7 @@
 import {test,expect} from "@playwright/test"
 
 test("launch amazon app", async({page})=>{
-    await page.goto("https://www.amazon.in/")
+    await page.goto("https://www.google.com/")
     await page.waitForTimeout(3000)
-    await expect(await page.title()).toContain("Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in")
+    await expect(await page.title()).toContain("Google")
 })
